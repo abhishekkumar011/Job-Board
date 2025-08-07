83eeb577121298c3991144bba1d79692495ff41a
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-08-07 16:09:22 +0530

    feat: add getJob API endpoint

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -20,8 +20,19 @@ const createJob = async (req, res) => {
     return res.status(200).json({ job, msg: "Job created successfully" });
   } catch (error) {
     console.error("Create Job Error:", error);
-    res.status(500).json({ msg: "Error while creating job" });
+    return res.status(500).json({ msg: "Error while creating job" });
   }
 };
 
-export { createJob };
+const getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({}).sort({ createdAt: -1 });
+
+    return res.status(200).json({ jobs, msg: "Jobs Fetched successfully" });
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+    return res.status(500).json({ msg: "Failed to fetch jobs" });
+  }
+};
+
+export { createJob, getJobs };
--- backend/src/routes/job.route.js
+++ backend/src/routes/job.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createJob } from "../controllers/job.controller.js";
+import { createJob, getJobs } from "../controllers/job.controller.js";
 
 const router = Router();
 
 router.route("/createjob").post(createJob);
+router.route("/").get(getJobs);
 
 export default router;
